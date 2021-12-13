196519d7e9bf21e169f0574d240b9133838b9a68
Hugo Protsch <hugo.protsch@gmail.com>
2021-12-13 22:08:25 +0100

    Add first twitter api test

---
+++ TwitterTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Parameters.V2;
+using Tweetinvi.Streaming.V2;
+
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets(Assembly.GetExecutingAssembly())
+    .Build();
+
+var userClient =
+    new TwitterClient(new ReadOnlyConsumerCredentials(config["consumerKey"], config["consumerSecret"], config["bearerToken"]));
+
+
+ISampleStreamV2 sampleStreamV2 = userClient.StreamsV2.CreateSampleStream();
+
+int tweets = 0;
+int events = 0;
+
+sampleStreamV2.TweetReceived += (_, x) => tweets++;
+sampleStreamV2.EventReceived += (_, x) => events++;
+
+Console.WriteLine("started stream");
+Stopwatch sw = Stopwatch.StartNew();
+sampleStreamV2.StartAsync(new StartSampleStreamV2Parameters());
+
+Task.Factory.StartNew(async () =>
+{
+    while (true)
+    {
+        await Task.Delay(5000);
+        float elapsedSeconds = sw.ElapsedMilliseconds / 1000f;
+        Console.WriteLine($"[TWEET] {tweets} ({tweets / elapsedSeconds}/s)");
+        Console.WriteLine($"[EVENT] {events} ({events / elapsedSeconds}/s)");
+        Console.WriteLine();
+        tweets = 0;
+        events = 0;
+        sw.Restart();
+    }
+});
+
+while (Console.ReadKey().Key != ConsoleKey.Q)
+{
+}
+
+sampleStreamV2.StopStream();
---
+++ TwitterTest/README.md
@@ -0,0 +1,7 @@
+﻿## Add secrets
+
+1. cd to `TwitterTest`
+2. add secrets
+    1. `dotnet user-secrets set "consumerKey" "..."`
+    2. `dotnet user-secrets set "consumerSecret" "..."`
+    3. `dotnet user-secrets set "bearerToken" "..."`
