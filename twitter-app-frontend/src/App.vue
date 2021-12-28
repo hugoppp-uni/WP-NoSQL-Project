73422f1a261efabc72d5d54485ba6451af9c3704
Alexander Koenemann <alexander.koenemann@xing.com>
2021-12-28 16:05:59 +0100

    Basic Frontend with simple input field created.

---
+++ twitter-app-frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Twitter logo" src="./assets/twitter_logo.png">
+    <StartPage msg="Twitter Hashtag Lookup App"/>
+  </div>
+</template>
+
+<script>
+import StartPage from './components/StartPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+   StartPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
